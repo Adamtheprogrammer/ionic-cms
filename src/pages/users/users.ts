3eb53040307b975a564e815d966195417b4aaebb
Adam-walker <quarterque@gmail.com>
2018-09-25 14:44:53 -0500

    Display a list of users

--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { UsersProvider } from '../../providers/users/users';
+import {User} from '../../models/user/user';
 
 /**
  * Generated class for the UsersPage page.
@@ -17,14 +18,26 @@ import { UsersProvider } from '../../providers/users/users';
 })
 export class UsersPage {
 
-  constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              private usersProvider:UsersProvider
+  users:User[];
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private usersProvider:UsersProvider
   ) {
   }
 
   ionViewDidLoad() {
-    this.usersProvider.getUsers();
+    this.getUsers();
   }
+  
+  private getUsers(): void{
+    this.usersProvider.getUsers().subscribe(
+      (response:any)=>{
+        this.users = response.users;
+        console.log(this.users);
+      }
 
+    );
+  }
 }
--- src/providers/users/users.ts
+++ src/providers/users/users.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import {Observable} from 'rxjs';
+
 import { User }from '../../models/user/user';
 /*
   Generated class for the UserProvider provider.
@@ -11,12 +13,14 @@ import { User }from '../../models/user/user';
 @Injectable()
 export class UsersProvider {
 
+  private url:string = 'http://localhost:3000/api/users';
+
   constructor(public http: HttpClient) {
     console.log('Hello UserProvider Provider');
   }
 
-  getUsers(): void{
-    console.log('all users');
+  getUsers(): Observable<User[]>{
+    return this.http.get<User[]>(this.url);
   }
 
 
@@ -36,10 +40,6 @@ export class UsersProvider {
   deleteUser(): void{
     console.log('delete');
   }
-
-
-
-
 
 
 }
